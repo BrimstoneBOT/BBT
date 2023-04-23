b7a31409018fe5f38f7111432f9f8df873a0e5ea
BrimstoneBOT <brimindustries@gmail.com>
2023-04-23 08:04:01 +0200

    5
    5

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,11 +1,50 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
   name: 'help',
-  description: 'Shows help message',
+  description: 'List all available commands',
   async execute(interaction) {
-    const helpEmbed = new MessageEmbed()
-      .setTitle('Help')
-      .setDescription('This is a help message.')
-      .setColor('#00ff00');
-    await interaction.reply({ embeds: [helpEmbed] });
+
+      const commandsList = [
+        {
+          name: '/about',
+          value: 'Get information about Brimstone!',
+        },
+        {
+          name: '/help',
+          value: 'List all available commands.',
+        },
+        {
+          name: '/clean',
+          value: 'Remove a specified number of messages from a channel. (Admin only)',
+        },
+        {
+          name: '/ping',
+          value: 'Check the bot\'s latency.',
+        },
+        {
+          name: '/eightball',
+          value: 'Ask a question and get a random answer!',
+        },
+        {
+          name: '/kiss',
+          value: 'Kiss another user! <3',
+        },
+        {
+          name: '/snipe',
+          value: 'Retrieve the last deleted message in the channel.',
+        },
+        {
+          name: '/audit',
+          value: 'Set up the audit system by creating an audit channel and assigning a role to the bot and user.',
+        },
+      ];
+
+      const helpEmbed = new MessageEmbed()
+        .setColor('#ff69b4')
+        .setTitle('BrimBot Help')
+        .addFields(commandsList);
+
+      await interaction.reply({ embeds: [helpEmbed] });
   },
 };
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,23 +1,21 @@
-async function handleInteractionCreate(interaction) {
+const { MessageEmbed } = require('discord.js');
+
+module.exports = async (interaction, client) => {
   if (!interaction.isCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
+
   if (!command) return;
 
   try {
     await command.execute(interaction);
   } catch (error) {
     console.error(error);
-    if (error.code === 'INTERACTION_NOT_REPLIED') {
-      await interaction.reply({
-        content: 'An error occurred while executing this command',
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: 'An error occurred while executing this command',
-        ephemeral: true,
-      });
-    }
+    const errorMessage = new MessageEmbed()
+      .setColor('#FF0000')
+      .setTitle('An error occurred while executing this command')
+      .setDescription('Please contact the bot owner if the issue persists.');
+
+    await interaction.reply({ embeds: [errorMessage], ephemeral: true });
   }
-}
+};
