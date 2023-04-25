fa1ed1398863b650e27c535001413ebe534bd51f
BrimstoneBOT <brimindustries@gmail.com>
2023-04-25 02:44:36 +0200

    20
    20

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -2,9 +2,16 @@ const GuildSettings = require('../models/guildSettings');
 
 module.exports = async (client, guild) => {
   try {
-    // Create new guild settings document in the database
-    const guildSettings = new GuildSettings({ guildId: guild.id });
-    await guildSettings.save();
+    // Check if the server configuration already exists in the database
+    const existingConfig = await GuildSettings.findOne({ guildId: guild.id });
+    if (!existingConfig) {
+      // If the configuration doesn't exist, create a new one
+      const guildSettings = new GuildSettings({ guildId: guild.id });
+      await guildSettings.save();
+      console.log(`Created new guild settings for server ${guild.name}`);
+    } else {
+      console.log(`Server ${guild.name} already has a config, skipping creation.`);
+    }
 
     // Register slash commands with new server
     const commands = await guild.commands.set(client.commands);
